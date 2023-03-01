f3112180963d80b117c30635d1ef173cedffd38f
Julius Sveikauskas <julius.sveikauskas@gmail.com>
2023-03-01 10:45:26 +0200

    signup

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,21 +1,74 @@
 import {LockClosedIcon} from '@heroicons/react/20/solid'
+import {Link} from "react-router-dom";
+import {useState} from "react";
+import axiosClient from "../axios.js";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
 
 export default function Signup() {
+    const {setToken, setCurrentUser} = useStateContext();
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passswordConfirmation, setPasswordConfirmation] = useState('');
+    const [error, setError] = useState({__html: ''});
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        setError({__html: ''})
+
+        axiosClient.post('/signup',{
+            name:fullName,
+            email,
+            password,
+            password_confirmation: passswordConfirmation
+        })
+            .then(({data}) => {
+                setToken(data.token);
+                setCurrentUser(data.user);
+            })
+            .catch((error) => {
+                if(error.response){
+                    const errors = Object.values(error.response.data.errors).reduce((accum, next) => [...accum, ...next],[])
+                    setError({__html: errors.join('<br/>')})
+                }
+            })
+    }
+
     return (
         <>
-
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Signup for free
             </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
                 Or{' '}
-                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                    start your 14-day free trial
-                </a>
+                <Link to="/login" className="font-medium text-indigo-600 hover:text-indigo-500">
+                    Login with your account
+                </Link>
             </p>
-            <form className="mt-8 space-y-6" action="#" method="POST">
+
+            {error.__html && (
+                <div className='bg-red-500 rounded py-2 px-3 text-white' dangerouslySetInnerHTML={error}></div>
+            )}
+
+
+            <form onSubmit={onSubmit} className="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" defaultValue="true"/>
                 <div className="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label htmlFor="full-name" className="sr-only">
+                            Full name
+                        </label>
+                        <input
+                            id="full-name"
+                            name="name"
+                            type="text"
+                            required
+                            value={fullName}
+                            onChange={ev => setFullName(ev.target.value)}
+                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Full name"
+                        />
+                    </div>
                     <div>
                         <label htmlFor="email-address" className="sr-only">
                             Email address
@@ -26,7 +79,9 @@ export default function Signup() {
                             type="email"
                             autoComplete="email"
                             required
-                            className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            value={email}
+                            onChange={ev => setEmail(ev.target.value)}
+                            className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                             placeholder="Email address"
                         />
                     </div>
@@ -40,29 +95,26 @@ export default function Signup() {
                             type="password"
                             autoComplete="current-password"
                             required
-                            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            value={password}
+                            onChange={ev => setPassword(ev.target.value)}
+                            className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                             placeholder="Password"
                         />
                     </div>
-                </div>
-
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <input
-                            id="remember-me"
-                            name="remember-me"
-                            type="checkbox"
-                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                        />
-                        <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                            Remember me
+                    <div>
+                        <label htmlFor="password-confirmation" className="sr-only">
+                            Password confirmation
                         </label>
-                    </div>
-
-                    <div className="text-sm">
-                        <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                            Forgot your password?
-                        </a>
+                        <input
+                            id="password-confirmation"
+                            name="password_confirmation"
+                            type="password"
+                            required
+                            value={passswordConfirmation}
+                            onChange={ev => setPasswordConfirmation(ev.target.value)}
+                            className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Password confirmation"
+                        />
                     </div>
                 </div>
 
@@ -74,7 +126,7 @@ export default function Signup() {
                 <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                   <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true"/>
                 </span>
-                        Sign in
+                        Signup
                     </button>
                 </div>
             </form>
